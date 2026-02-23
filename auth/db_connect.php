eaa190cc8ca4ad34f8ccb98c5e678c1de85bc220
burekrurek <=>
2026-02-23 21:03:24 +0100

    Created simple db connection file

--- auth/db_connect.php
+++ auth/db_connect.php
@@ -1,7 +1,40 @@
-<?php 
-$conn = mysqli_connect('localhost','usr1_adm', PASSWORD, 'eDzienniczek');
-if (!$conn){
-    die();
-}
+<?php
 
-?>
+function db_connect($db_key) {
+
+    $databases = [
+        'main' => [
+            'host' => 'localhost',
+            'user' => 'main_adm',
+            /*TODO: env variable with passwords */ 
+            'pass' => PASSWORD,
+            'name' => 'main'
+        ],
+        'users' => [
+            'host' => 'localhost',
+            'user' => 'usrs_adm',
+            'pass' => PASSWORD,
+            'name' => 'users_db'
+        ],
+    ];
+
+
+    if (!isset($databases[$db_key])) {
+        die("Database key does not exist.");
+    }
+
+    $config = $databases[$db_key];
+
+    $conn = mysqli_connect(
+        $config['host'],
+        $config['user'],
+        $config['pass'],
+        $config['name']
+    );
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $conn;
+}
